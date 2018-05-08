c5ea6dc5b06367cac689947d25929e43c214b033
git-palace <edeveloper3@gmail.com>
2018-05-08 16:11:58 -0700

    updates

--- index.php
+++ index.php
@@ -9,27 +9,34 @@
 
 	<title>Clarity Movement Co. - Leading the Clean Air Movement</title>
 	<meta name="description" content="Transform your city using hundreds to thousands of IoT devices collecting actionable air quality data to create healthy communities.">
-	<meta name="twitter:description" content="Transform your city using hundreds to thousands of IoT devices collecting actionable air quality data to create healthy communities.">
-	<meta name="twitter:title" content="Clarity Movement Co. - Leading the Clean Air Movement">
 	<meta property="og:description" content="Transform your city using hundreds to thousands of IoT devices collecting actionable air quality data to create healthy communities.">
 	<meta property="og:title" content="Clarity Movement Co. - Leading the Clean Air Movement">
-    <meta property="og:image" content="/assets/images/share-1.jpg">
-	<meta name="twitter:image" content="/assets/images/share-1.jpg">
-
-    <meta property="og:site_name" content="Clarity Movement Co.">
-    <meta property="og:url" content="https://clarity.io/" >
+	<meta property="og:image" content="/assets/images/share-1.jpg">
+	<meta property="og:site_name" content="Clarity Movement Co.">
+	<meta property="og:url" content="https://clarity.io/" >
 	<meta property="og:type" content="website">
+
+	<meta name="twitter:title" content="Clarity Movement Co. - Leading the Clean Air Movement">
+	<meta name="twitter:description" content="Transform your city using hundreds to thousands of IoT devices collecting actionable air quality data to create healthy communities.">
+	<meta name="twitter:image" content="/assets/images/share-1.jpg">
 	<meta name="twitter:card" content="summary">
 	<meta name="twitter:creator" content="@JoinClarity">
 	<meta name="twitter:site" content="@JoinClarity">
 
-
-
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
-	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<meta name="theme-color" content="#37a5fb">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="ms-icon-144x144.png">
+
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
+	<link rel="mask-icon" href="/assets/images/safari-pinned-tab.svg" color="#37a5fb">
+
 	<link rel="apple-touch-icon" href="/assets/images/apple-touch-icon.png" />
 	<link rel="apple-touch-icon" sizes="57x57" href="/assets/images/apple-touch-icon-57x57.png" />
 	<link rel="apple-touch-icon" sizes="72x72" href="/assets/images/apple-touch-icon-72x72.png" />
@@ -39,31 +46,18 @@
 	<link rel="apple-touch-icon" sizes="144x144" href="/assets/images/apple-touch-icon-144x144.png" />
 	<link rel="apple-touch-icon" sizes="152x152" href="/assets/images/apple-touch-icon-152x152.png" />
 	<link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon-180x180.png" />
-
-	<link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
-	<link rel="manifest" href="/assets/js/manifest.json">
-	<link rel="mask-icon" href="/assets/images/safari-pinned-tab.svg" color="#37a5fb">
-	<meta name="theme-color" content="#37a5fb">
-
-	<link rel="manifest" href="manifest.json">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="ms-icon-144x144.png">
-	<meta name="theme-color" content="#ffffff">
-	
 	<link type="text/css" media="all" href="/assets/css/styles.css?v=5" rel="stylesheet" />
+	<link rel="manifest" href="/assets/js/manifest.json">
 
 	<script>
 		//remove no-js class to make sure css animations are not bypassed
 		document.getElementsByTagName( 'html' )[0].classList.remove('no-js');
 		document.getElementsByTagName( 'html' )[0].classList.add('has-js');
 	</script>
-
 </head>
 <body>
 	
-	<x-application ws-root="/assets/images//clarity-io.herokuapp.com/api/data/" class="u-block"></x-application>
+	<x-application ws-root="//clarity-io.herokuapp.com/api/data/" class="u-block"></x-application>
 	
 	<script>
 			document.querySelector('x-application').innerHTML = '';
